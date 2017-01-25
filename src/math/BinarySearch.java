0b2001a79f38656cc47f389ba2aa829edf5460ad
黄荣君 <l1091462907@gmail.com>
2017-01-25 21:37:41 +0800

    更新函数名称

--- src/math/BinarySearch.java
+++ src/math/BinarySearch.java
@@ -9,11 +9,11 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 8, 99, 7, 65, 44};
         Arrays.sort(arr);
-        System.out.println(rank(5,arr));
+        System.out.println(indexOf(5, arr));
     }
 
 
-    public static int rank(int key, int[] arr) {
+    public static int indexOf(int key, int[] arr) {
         int lo = 0;
         int hi = arr.length - 1;
         while (lo <= hi) {
