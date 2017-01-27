fff640ec873b7b6ca9698c4cafba7b4d059782b0
黄荣君 <l1091462907@gmail.com>
2017-01-27 20:51:43 +0800

    计算平方根（牛顿迭代法）

--- src/math/Util.java
+++ src/math/Util.java
@@ -37,5 +37,24 @@ public class Util {
         }
         return true;
     }
+
+    /**
+     * 计算平方根（牛顿迭代法）
+     *
+     * @param c
+     * @return
+     */
+    public static double sqrt(double c) {
+        if (c < 0) {
+            return Double.NaN;
+        }
+        double err = 1e-15;
+        double t = c;
+        while (Math.abs(t - c / t) > err * t) {
+            t = (c / t + t) / 2.0;
+        }
+        return t;
+    }
+
 }
 
