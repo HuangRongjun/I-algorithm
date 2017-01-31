944c646ba273d70e58ee5c0b059655b29315a116
黄荣君 <l1091462907@gmail.com>
2017-01-31 22:03:09 +0800

    增加矩阵计算

---
+++ src/math/Matrix.java
@@ -0,0 +1,23 @@
+package math;
+
+/**
+ * Created by lancer on 2017/1/31.
+ */
+public class Matrix {
+
+    /**
+     * 点向量相乘
+     * @param x
+     * @param y
+     * @return
+     */
+    public static double dot(double[] x, double[] y) {
+        double sum = 0.0;
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < y.length; j++) {
+                sum += x[i] * y[j];
+            }
+        }
+        return sum;
+    }
+}
