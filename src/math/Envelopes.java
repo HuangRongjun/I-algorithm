c13be5d21865575cdfe4c6c9bf772cd6646bf509
黄荣君 <l1091462907@gmail.com>
2017-02-05 22:27:13 +0800

    模拟微信抢红包的算法，随机获取钱的算法待改进 ?

---
+++ src/math/Envelopes.java
@@ -0,0 +1,24 @@
+package math;
+
+import util.StdOut;
+import util.StdRandom;
+
+/**
+ * Created by lancer on 2017/2/5.
+ */
+public class Envelopes {
+    public static void main(String[] args) {
+        double total = 10;
+        int num = 8;
+        double min = 0.01;
+        for (int i = 1; i < num; i++) {
+            //随机安全上线值待改进
+            double max = (total - (num - i) * min) / (num - i);
+            //TODO 随机获取钱的算法待改进
+            double money = StdRandom.uniform(min, max);
+            total -= money;
+            StdOut.println(String.format("第 %d 个红包，%f 元，余额 %f 元", i, money, total));
+        }
+        StdOut.println(String.format("第 %d 个红包，%f 元，余额 0 元", num, total));
+    }
+}
