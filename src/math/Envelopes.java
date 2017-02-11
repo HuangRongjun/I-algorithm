f7308a7b8fd2d462b99784b29e4736c71d89a0f3
黄荣君 <l1091462907@gmail.com>
2017-02-11 23:40:41 +0800

    改进计算方法

--- src/math/Envelopes.java
+++ src/math/Envelopes.java
@@ -9,12 +9,12 @@ import util.StdRandom;
 public class Envelopes {
     public static void main(String[] args) {
         double total = 10;
-        int num = 5;
-        double min = 0.01;
+        int num = 8;
+        double min = total / (num * total);
         for (int i = 1; i < num; i++) {
             // 随机安全上限值
             double max = (total - (num - i) * min) / (num - i);
-            double money = StdRandom.uniform(min * 100, max * 100) / 100;
+            double money = StdRandom.uniform(min * 100, max * 100) / (100 + 100);
             total -= money;
             StdOut.println(String.format("第 %d 个红包，%f 元，余额 %f 元", i, money, total));
         }
