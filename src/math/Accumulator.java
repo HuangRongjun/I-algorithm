5b1c526667bf82c2e696d532d1a63b0e1c586ada
黄荣君 <l1091462907@gmail.com>
2017-02-19 15:34:59 +0800

    计算一组数据的实时平均值

---
+++ src/math/Accumulator.java
@@ -0,0 +1,26 @@
+package math;
+
+/**
+ * 计算一组数据的实时平均值
+ * <p>
+ * Created by lancer on 2017/2/19.
+ */
+public class Accumulator {
+    private double total;
+    private int N;
+
+    public void addDataValue(double value) {
+        N++;
+        total += value;
+    }
+
+    public double mean() {
+        return total / N;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Mean(%d Values): %7.5f", N, mean());
+    }
+
+}
---
+++ src/math/AccumulatorTest.java
@@ -0,0 +1,19 @@
+package math;
+
+import alg4.StdRandom;
+import util.StdOut;
+
+/**
+ * Created by lancer on 2017/2/19.
+ */
+public class AccumulatorTest {
+    public static void main(String[] args) {
+        int T = Integer.parseInt(args[0]);
+        Accumulator acc = new Accumulator();
+        for (int i = 0; i < T; i++) {
+            acc.addDataValue(StdRandom.random());
+        }
+        StdOut.println(acc);
+    }
+
+}
